9b6e79002b9948236cbd2aa2da7b26aaf5edfac9
Ezra Peisach <ezra.peisach@rcsb.org>
2020-02-02 20:13:09 -0500

    Add tests for write caches

--- wwpdb/utils/tests-config/ConfigInfoFileExecTests.py
+++ wwpdb/utils/tests-config/ConfigInfoFileExecTests.py
@@ -23,7 +23,7 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(HERE)))
 TESTOUTPUT = os.path.join(HERE, "test-output", platform.python_version())
 if not os.path.exists(TESTOUTPUT):
-    os.makedirs(TESTOUTPUT)
+    os.makedirs(TESTOUTPUT)  # pragma: no cover
 mockTopPath = os.path.join(TOPDIR, "wwpdb", "mock-data")
 rwMockTopPath = os.path.join(TESTOUTPUT)
 
@@ -51,7 +51,7 @@ class ConfigInfoFileExecTests(unittest.TestCase):
         pass
 
     def testConfigPath(self):
-        cif = ConfigInfoFileExec()
+        cif = ConfigInfoFileExec(mockTopPath=mockTopPath)
 
         status = cif.testConfigPath()
         self.assertTrue(status, "testconfig read access")
@@ -59,10 +59,50 @@ class ConfigInfoFileExecTests(unittest.TestCase):
         self.assertTrue(status, "testconfig write access")
 
     def testPrintConfig(self):
-        cif = ConfigInfoFileExec()
+        cif = ConfigInfoFileExec(mockTopPath=mockTopPath)
         # Test coverage
         cif.printConfig("rcsb-east", "WWPDB_DEPLOY_TEST")
 
+    def testWriteConfig(self):
+        """Test writing config file"""
+        subtestdir = os.path.join(TESTOUTPUT, "testconfig")
+        testout = os.path.join(subtestdir, "site-config", "rcsb-east", "wwpdb_deploy_test", "ConfigInfoFileCache.json")
+        if os.path.exists(testout):
+            os.remove(testout)
+        cr = CreateRWTree(mockTopPath, subtestdir)
+        cr.createtree(["site-config"])
+        saveconf = os.environ["TOP_WWPDB_SITE_CONFIG_DIR"]
+        try:
+            os.environ["TOP_WWPDB_SITE_CONFIG_DIR"] = os.path.join(subtestdir, "site-config")
+            cif = ConfigInfoFileExec(mockTopPath=subtestdir)
+            cif.writeConfigCache(siteLoc="rcsb-east", siteId="WWPDB_DEPLOY_TEST")
+        except Exception as e:  # pragma: no cover
+            self.fail("Error testing writing config %s" % str(e))
 
-if __name__ == "__main__":
+        os.environ["TOP_WWPDB_SITE_CONFIG_DIR"] = saveconf
+
+        self.assertTrue(os.path.exists(testout))
+
+    def testWriteLocationConfig(self):
+        """Test writing config file"""
+        subtestdir = os.path.join(TESTOUTPUT, "testconfig")
+        testout = os.path.join(subtestdir, "site-config", "rcsb-east", "wwpdb_deploy_test", "ConfigInfoFileCache.json")
+        if os.path.exists(testout):
+            os.remove(testout)
+        cr = CreateRWTree(mockTopPath, subtestdir)
+        cr.createtree(["site-config"])
+        saveconf = os.environ["TOP_WWPDB_SITE_CONFIG_DIR"]
+        try:
+            os.environ["TOP_WWPDB_SITE_CONFIG_DIR"] = os.path.join(subtestdir, "site-config")
+            cif = ConfigInfoFileExec(mockTopPath=subtestdir)
+            cif.writeLocationConfigCache(siteLoc="rcsb-east")
+        except Exception as e:  # pragma: no cover
+            self.fail("Error testing writing config %s" % str(e))
+
+        os.environ["TOP_WWPDB_SITE_CONFIG_DIR"] = saveconf
+
+        self.assertTrue(os.path.exists(testout))
+
+
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
