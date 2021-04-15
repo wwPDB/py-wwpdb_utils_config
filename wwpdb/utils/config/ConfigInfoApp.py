b02ef61e2e4c49113c564db1f350506ff36728c7
jmb <jmb@ebi.ac.uk>
2021-04-15 08:21:00 +0100

    added access for archive current dictionary

--- wwpdb/utils/config/ConfigInfoApp.py
+++ wwpdb/utils/config/ConfigInfoApp.py
@@ -53,13 +53,19 @@ class ConfigInfoAppBase(object):
             self._referencedir = self._cI.get("REFERENCE_PATH")
         return self._referencedir
 
-    def _get_mmcif_deposit_dict_filename(self):
-        return self._cI.get("PDBX_DICTIONARY_NAME_DICT", {}).get('DEPOSIT')
+    def _get_pdbx_dictionary_name_dict(self):
+        return self._cI.get("PDBX_DICTIONARY_NAME_DICT", {})
 
     def __warndeprecated(self, msg):
         """Logs warning message"""
         # stacklevel is to get up high enough to get caller
         warnings.warn(msg, DeprecationWarning, stacklevel=4)
+
+    def get_mmcif_deposit_dict_filename(self):
+        return self._get_pdbx_dictionary_name_dict().get('DEPOSIT')
+
+    def get_mmcif_archive_current_dict_filename(self):
+        return self._get_pdbx_dictionary_name_dict().get('ARCHIVE_CURRENT')
 
 
 class ConfigInfoAppCommon(ConfigInfoAppBase):
@@ -74,7 +80,7 @@ class ConfigInfoAppCommon(ConfigInfoAppBase):
 
     def get_mmcif_next_dictionary_file_path(self):
 
-        mmcif_dictionary_name = self._get_mmcif_deposit_dict_filename()
+        mmcif_dictionary_name = self.get_mmcif_deposit_dict_filename()
         mmcif_dictionary_file_name = mmcif_dictionary_name + '.dic'
         newpath = os.path.join(self.get_mmcif_dict_path(), mmcif_dictionary_file_name)
         return self._getlegacy("SITE_MMCIF_DICT_FILE_PATH", newpath)
