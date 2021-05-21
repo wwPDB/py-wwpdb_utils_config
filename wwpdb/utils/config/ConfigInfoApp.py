c0de481d50406cc18ee5e92da5316608e7b46cb3
jmb <jmb@ebi.ac.uk>
2021-05-21 21:40:35 +0100

    corrected path for top web apps

--- wwpdb/utils/config/ConfigInfoApp.py
+++ wwpdb/utils/config/ConfigInfoApp.py
@@ -75,7 +75,7 @@ class ConfigInfoAppBase(object):
 
     def _get_site_web_apps_top_path(self):
         if self._top_webapps_path is None:
-            self._top_webapps_path = self._cI.get("TOP_WWPDB_WEBAPPS_DIR")
+            self._top_webapps_path = self._cI.get("SITE_WEB_APPS_TOP_PATH")
         return self._top_webapps_path
 
     def get_site_packages_path(self):
