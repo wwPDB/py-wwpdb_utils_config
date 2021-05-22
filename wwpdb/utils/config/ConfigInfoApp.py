f35debbd851be531e435c10d952120a47ce4f3ec
John Berrisford <jmb@ebi.ac.uk>
2021-05-22 20:08:32 +0100

    make SITE_WEB_APPS_TOP_PATH and TOP_WWPDB_WEBAPPS_DIR the same
    make SITE_WEB_APPS_TOP_PATH and TOP_WWPDB_WEBAPPS_DIR the same

--- wwpdb/utils/config/ConfigInfoApp.py
+++ wwpdb/utils/config/ConfigInfoApp.py
@@ -118,7 +118,8 @@ class ConfigInfoAppCommon(ConfigInfoAppBase):
         return self._get_site_local_apps()
 
     def get_site_web_apps_top_path(self):
-        return self._getlegacy("SITE_WEB_APPS_TOP_PATH", os.path.join(self._get_top_web_apps_top_path(), "webapps"))
+        # return self._getlegacy("SITE_WEB_APPS_TOP_PATH", os.path.join(self._get_top_web_apps_top_path(), "webapps"))
+        return self._getlegacy("SITE_WEB_APPS_TOP_PATH", self._get_top_web_apps_top_path())
 
     def get_site_annot_tools_path(self):
         return self._getlegacy("SITE_ANNOT_TOOLS_PATH", os.path.join(self.get_site_packages_path(), "annotation"))
